a9afdaff8fc932e9ba415c5639c28805539483bf
jonnydinesh <111968830+jonnydinesh@users.noreply.github.com>
2023-02-25 21:56:14 +0530

    Add files via upload

---
+++ Abstact.java
@@ -0,0 +1,22 @@
+abstract class Sports
+{
+    public void india();
+    public void england();
+    public void southAfrica();
+    public void Australia()
+    {
+        String name="rickyponting";
+        String captain="australia"
+        String battingStyle="right hand batsaman"
+        System.out.println(name+""+captain+""+battingStyle);
+    }
+    
+}
+
+
+
+
+
+
+
+
